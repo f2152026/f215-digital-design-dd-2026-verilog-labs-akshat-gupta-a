9e5cc4ad270fa81ac20a46358fde185e346fd000
Akshat-Gupta-a <f20241044@pilani.bits-pilani.ac.in>
2026-08-29 17:48:34 +0000

    lab01 task4

--- labs/lab01/task4/cla4.v
+++ labs/lab01/task4/cla4.v
@@ -29,6 +29,7 @@ module cla4(
   output       cout
 );
 
+
   // TODO: your gate-level P/G, carry, and sum logic goes here.
   // (cout should be connected to c4.) Remember the delay on every gate.
 
